94fb026b033944ddb4f6161bdbfc66b220a5daf3
Július Retzer <jul.retzer@gmail.com>
2016-06-06 08:50:55 +0200

    Fix lint errors

--- client/components/item.jsx
+++ client/components/item.jsx
@@ -1,27 +1,25 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
-const TodoItem = ({ todo, handleComplete, handleDelete }) => {
-  return (
-    <li className={classNames({ completed: todo.completed })}>
-      <div className="view">
-        <input
-          className="toggle"
-          type="checkbox"
-          checked={todo.completed}
-          onChange={() => handleComplete(todo)}
-        />
-        <label>
-          {todo.title}
-        </label>
-        <button className="destroy" onClick={() => handleDelete(todo)}/>
-      </div>
+const TodoItem = ({ todo, handleComplete, handleDelete }) => (
+  <li className={classNames({ completed: todo.completed })}>
+    <div className="view">
       <input
-        className="edit"
+        className="toggle"
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => handleComplete(todo)}
       />
-    </li>
-  );
-};
+      <label>
+        {todo.title}
+      </label>
+      <button className="destroy" onClick={() => handleDelete(todo)} />
+    </div>
+    <input
+      className="edit"
+    />
+  </li>
+);
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
--- client/server.js
+++ client/server.js
@@ -1,15 +1,17 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+  historyApiFallback: true,
+})
+.listen(3000, 'localhost', (err) => {
   if (err) {
-    return console.log(err);
+    console.log(err); // eslint-disable-line no-console
+    return;
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://localhost:3000/'); // eslint-disable-line no-console
 });
